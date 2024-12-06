594bfea507627db18011a126da77d0e8bae1b384
yiiiiing <ruiyingyang0408@gmail.com>
2024-12-06 16:33:25 +0200

    init mongoose

---
+++ backend/mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://dinosaur:${password}@cluster0.vx8ie.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Persons', personSchema)
+
+
+// print all persons 
+if (process.argv.length < 4){
+    Person.find({}).then(result => {
+        result.forEach(note => {
+          console.log(note)
+        })
+        mongoose.connection.close()
+    })
+}
+
+if (process.argv.length > 3){
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+      })
+      
+      person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+      })
+      
+}
+
